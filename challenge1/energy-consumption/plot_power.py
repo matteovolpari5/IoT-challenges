6a3e59881199b974bc6028ea6086f60ac254f423
kevinziroldi <k.ziroldi@gmail.com>
2025-03-14 12:49:02 +0100

    added power consumption plots in pdf version

--- challenge1/energy-consumption/plot_power.py
+++ challenge1/energy-consumption/plot_power.py
@@ -2,13 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # read CSV file 
-df = pd.read_csv('transmission_power.csv', parse_dates=['Timestamp'])
+df = pd.read_csv('deep_sleep.csv', parse_dates=['Timestamp'])
 
 # plot data
 plt.figure(figsize=(10, 6))
 plt.plot(df['Timestamp'], df['Data'], linestyle='-', linewidth=2)
 plt.xlabel('Time')
 plt.ylabel('Power [mW]')
-plt.title('Power consumption transmission')
+plt.title('Power consumption deep sleep')
 plt.grid(True)
+
+# save and shows
+plt.savefig("power_consumption_deep_sleep.pdf", format="pdf", bbox_inches="tight")
 plt.show()
